05279081d0e193e71d40c31a23c01dbb115af925
CharlieHii <charlie_chaper@hotmail.com>
2024-05-19 20:43:09 +0700

    feat(index.html): add active feature

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
       veryDarkBlue: "hsl(212, 21%, 14%)",
       darkGrayishBlue: "hsl(228, 12%, 48%)",
       white: "hsl(0, 0%, 100%)",
+      veryDarkCyan: "hsl(158, 43%, 18%)",
     },
     fontFamily: {
       Montserrat: ["Montserrat"],
