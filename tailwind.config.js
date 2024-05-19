fd33fc9c5225d8459049d4f9a863244eecd1dc48
CharlieHii <charlie_chaper@hotmail.com>
2024-05-19 18:37:19 +0700

    feat,style(index.html,tailwind.config.js): complete code structure and style

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    colors: {
+      darkCyan: "hsl(158, 36%, 37%)",
+      cream: "hsl(30, 38%, 92%)",
+      veryDarkBlue: "hsl(212, 21%, 14%)",
+      darkGrayishBlue: "hsl(228, 12%, 48%)",
+      white: "hsl(0, 0%, 100%)",
+    },
+    fontFamily: {
+      Montserrat: ["Montserrat"],
+      Fraunces: ["Fraunces"],
+    },
+  },
+};
