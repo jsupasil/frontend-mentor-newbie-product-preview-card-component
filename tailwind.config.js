3fefe5b16aab0ad36ccc0bd781fb575a41bc0141
CharlieHii <charlie_chaper@hotmail.com>
2024-05-19 21:48:44 +0700

    style(index.html): add shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   theme: {
-    colors: {
-      darkCyan: "hsl(158, 36%, 37%)",
-      cream: "hsl(30, 38%, 92%)",
-      veryDarkBlue: "hsl(212, 21%, 14%)",
-      darkGrayishBlue: "hsl(228, 12%, 48%)",
-      white: "hsl(0, 0%, 100%)",
-      veryDarkCyan: "hsl(158, 43%, 18%)",
-    },
-    fontFamily: {
-      Montserrat: ["Montserrat"],
-      Fraunces: ["Fraunces"],
+    extend: {
+      colors: {
+        darkCyan: "hsl(158, 36%, 37%)",
+        cream: "hsl(30, 38%, 92%)",
+        veryDarkBlue: "hsl(212, 21%, 14%)",
+        darkGrayishBlue: "hsl(228, 12%, 48%)",
+        white: "hsl(0, 0%, 100%)",
+        veryDarkCyan: "hsl(158, 43%, 18%)",
+      },
+      fontFamily: {
+        Montserrat: ["Montserrat"],
+        Fraunces: ["Fraunces"],
+      },
     },
   },
 };
